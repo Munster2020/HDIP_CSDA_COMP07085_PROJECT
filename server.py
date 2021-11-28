bccd94cc4e785e0ae3fb2084035fc038635214fc
Munster2020 <g00387820@gmit.ie>
2021-11-28 09:55:49 +0000

    Code changes to Server.py

--- server.py
+++ server.py
@@ -5,15 +5,17 @@ from MoviesDao import moviesDao
 
 app = Flask(__name__, static_url_path='', static_folder='template')
 
-
+# curl "http://127.0.0.1:5000/dvds"
 @app.route('/dvds')
 def getAll():
     return jsonify(moviesDao.getAll())
 
+# curl "http://127.0.0.1:5000/dvds/1"
 @app.route('/dvds/<int:id>')
 def findById(id):
     return jsonify(moviesDao.findById(id))
 
+# curl  -i -H "Content-Type:application/json" -X POST -d "{\"id\":\"89\",\"title\":\"The Hateful Eight\",\"director\":\"Quentin Tarantino\",\"genre\":\"Western\", \"price\":14}" http://127.0.0.1:5000/dvds
 @app.route('/dvds', methods=['POST'])
 def create():
     if not request.json:
@@ -28,6 +30,7 @@ def create():
     }
 
     return jsonify(moviesDao.create(dvd))
+
 
 @app.route('/dvds/<int:id>', methods=['PUT'])
 def update(id):
@@ -47,6 +50,7 @@ def update(id):
     moviesDao.update(currentDvd)
     return jsonify(currentDvd)
 
+# curl -X DELETE http://127.0.0.1:5000/dvds/87
 @app.route('/dvds/<int:id>', methods=['DELETE'])
 def delete(id):
     moviesDao.delete(id)
